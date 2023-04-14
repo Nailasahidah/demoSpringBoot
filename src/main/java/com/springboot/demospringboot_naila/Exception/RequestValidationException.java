fe315cf88eeab17f12c6972a18e56739b1131168
LAB 2B-06 <nailasahidah017@gmail.com>
2023-04-14 03:43:27 -0500

    12

--- src/main/java/com/springboot/demospringboot_naila/Exception/RequestValidationException.java
+++ src/main/java/com/springboot/demospringboot_naila/Exception/RequestValidationException.java
@@ -6,7 +6,6 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 @ResponseStatus(HttpStatus.BAD_REQUEST)
 public class RequestValidationException extends  RuntimeException{
     public RequestValidationException(String message) {
-
         super(message);
     }
 }
